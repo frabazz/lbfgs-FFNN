8415781785144d99840985d4132cbcf33d1324f2
buzz <francesco260403@gmail.com>
2025-12-20 20:38:51 +0100

    working autodiff

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,23 @@
-#include <autodiff/forward/dual.hpp>
+#include <iostream>
+#include <autodiff/reverse/var.hpp>
+#include <autodiff/reverse/var/eigen.hpp>
 
-int main(){}
+using namespace autodiff;
+
+// Se vuoi usare VectorXd originale di Eigen
+// #include <Eigen/Core> 
+
+var f(const ArrayXvar& x) {
+  return (x * x).sum(); // Nota: usa .sqrt() o autodiff::sqrt
+}
+
+int main() {
+    VectorXvar x(5);
+    x << 1, 2, 3, 4, 5;
+    
+    var y = f(x);
+    Eigen::VectorXd dydx = gradient(y, x);
+    
+    std::cout << "dy/dx = \n" << dydx << std::endl;
+    return 0;
+}
